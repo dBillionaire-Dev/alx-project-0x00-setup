a2879e3dab6b52b6069ab309562a532ac3c582bb
dBillioniare-Dev <nezerekunke@gmail.cm>
2025-05-30 20:55:06 +0100

    task_2

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const landing: React.FC = () => {
+    return (
+        <div>
+            <h1 className=" text-xl font-extralight">Landing Page</h1>
+        </div>
+    )
+}
+
+export default landing;
